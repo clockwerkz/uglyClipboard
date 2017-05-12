90451990e71a2eb615cb1e86412fa6ac0c410652
QbanCowboy <qbancowboy@qbancowboy.com>
2017-05-11 19:56:21 -0700

    Started building out the timer function using setInterval and clearInterval

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-console.log("Works");
+//console.log("Works");
+var time = 5;
+function countdown() {
+	if (time===0) {
+		clearInterval(timerCountdown);
+		console.log("Time's up!");
+	}
+	else {
+		console.log(time--);
+	}
+}
+
+var timerCountdown = setInterval(countdown, 1000);
