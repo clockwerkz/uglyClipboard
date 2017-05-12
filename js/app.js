6c4a035fc492e137424259fcb6deededcd51b5db
Carlos <clockwerkz@gmail.com>
2017-05-11 23:55:03 -0700

    Created the css file and started basic styling:

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 //console.log("Works");
+const stopwatch = document.querySelector('#stopwatch');
+console.log(stopwatch);
 var time = 5;
 function countdown() {
 	if (time===0) {
 		clearInterval(timerCountdown);
+		stopwatch.innerHTML="FINISHED";
 		console.log("Time's up!");
 	}
 	else {
+		stopwatch.innerHTML=time;
 		console.log(time--);
 	}
 }
