0520e2b7ec0e1128c826822b8d822d47ccd55758
Carlos <clockwerkz@gmail.com>
2017-05-14 15:01:51 -0700

    Trying to figure out SetInterval and how to use it in this app

--- js/app.js
+++ js/app.js
@@ -1,17 +1,23 @@
 //console.log("Works");
 const stopwatch = document.querySelector('#stopwatch');
 console.log(stopwatch);
+var totalTime = 2;
+var timerCountdown;
+var active = false;
 var time = 5;
+continueTimer = false;
 function countdown() {
 	if (time===0) {
 		clearInterval(timerCountdown);
 		stopwatch.innerHTML="FINISHED";
 		console.log("Time's up!");
+		active = false;
+		continueTimer = true;
 	}
 	else {
 		stopwatch.innerHTML=time;
 		console.log(time--);
 	}
 }
+console.log("starting");
 
-var timerCountdown = setInterval(countdown, 1000);
