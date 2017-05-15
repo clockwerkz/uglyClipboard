8ae5c917735812f59e2e70cedbddf10daa1b2894
QbanCowboy <qbancowboy@qbancowboy.com>
2017-05-14 18:42:44 -0700

    Figured out how to use the setInterval method, now I just need to add a form that will ask how many workouts, desired length of time per workout, and desired rest break between sets. Also, I'd like to add a pause button, but that will require some investigating.

--- js/app.js
+++ js/app.js
@@ -1,23 +1,31 @@
 //console.log("Works");
 const stopwatch = document.querySelector('#stopwatch');
 console.log(stopwatch);
-var totalTime = 2;
-var timerCountdown;
-var active = false;
 var time = 5;
-continueTimer = false;
+var rest = false;
+var masterTime=2;
 function countdown() {
-	if (time===0) {
+	stopwatch.innerHTML = time;
+	if (time===0){
+		if (rest) {
+			masterTime--;
+			console.log("starting workout: "+masterTime);
+			rest = !rest;
+			time = 5;
+		} else {
+			console.log ("Rest Period");
+			rest = !rest;
+			time = 3;
+		}
+	} else {
+		time--;
+	}
+	if (masterTime===0) {
 		clearInterval(timerCountdown);
 		stopwatch.innerHTML="FINISHED";
 		console.log("Time's up!");
-		active = false;
-		continueTimer = true;
-	}
-	else {
-		stopwatch.innerHTML=time;
-		console.log(time--);
 	}
 }
-console.log("starting");
+console.log("starting workout: "+masterTime);
+var timerCountdown = setInterval(countdown, 1000);
 
