5bd0561177df20c682bb79d81d53e3e3685be2f0
Carlos <clockwerkz@gmail.com>
2017-05-14 19:56:47 -0700

    Added sound effects to last 3 seconds. Still need to figure out how to keep it from displaying undefined at the end of the work out

--- js/app.js
+++ js/app.js
@@ -1,31 +1,46 @@
 //console.log("Works");
 const stopwatch = document.querySelector('#stopwatch');
+const workoutName = document.querySelector('#workoutName');
+const lowBeep = document.querySelector('#lowBeep');
+const highBeep = document.querySelector('#highBeep');
 console.log(stopwatch);
-var time = 5;
+repTime = 15;
+restTime = 5;
+var time = repTime;
 var rest = false;
-var masterTime=2;
+var isDone = false;
+const workouts = ["Jumping Jacks", "Push Ups", "Bear Crawls"]
+var masterTime=workouts.length;
 function countdown() {
 	stopwatch.innerHTML = time;
-	if (time===0){
-		if (rest) {
+	if (time===2 || time===1) {
+		lowBeep.play();
+	}
+	if (time===0 && !isDone){
+		highBeep.play();
+		if (rest && !isDone) {
 			masterTime--;
+			workoutName.innerHTML = workouts[workouts.length-masterTime];
 			console.log("starting workout: "+masterTime);
 			rest = !rest;
-			time = 5;
+			time = repTime;
 		} else {
+			workoutName.innerHTML = "Rest Break";
 			console.log ("Rest Period");
 			rest = !rest;
-			time = 3;
+			time = restTime;
 		}
 	} else {
 		time--;
 	}
 	if (masterTime===0) {
+		isDone = true;
 		clearInterval(timerCountdown);
 		stopwatch.innerHTML="FINISHED";
 		console.log("Time's up!");
 	}
 }
 console.log("starting workout: "+masterTime);
+workoutName.innerHTML = workouts[workouts.length-masterTime];
 var timerCountdown = setInterval(countdown, 1000);
 
