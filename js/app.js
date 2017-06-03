eddf92965045b41f88c59e9eac3679602a140a6e
Carlos <clockwerkz@gmail.com>
2017-06-02 21:15:11 -0700

    Using p5 sound library to test if sounds will play on ios devices.

--- js/app.js
+++ js/app.js
@@ -2,15 +2,20 @@
 var stopwatch = document.getElementById('stopwatch');
 var workoutName = document.getElementById('workoutName');
 
-var lowBeep = new Audio('sounds/low_beep.wav');
-var highBeep = new Audio('sounds/high_beep.wav');
-repTime = 15;
-restTime = 5;
+var lowBeep,highBeep;
+var repTime = 5;
+var restTime = 5;
 var time = repTime;
 var rest = false;
 var isDone = false;
 var workouts = ["Jumping Jacks", "Push Ups", "Bear Crawls"]
 var masterTime=workouts.length;
+
+function setup(){
+	lowBeep = loadSound('sounds/low_beep.wav');
+	highBeep = loadSound('sounds/high_beep.wav');
+}
+
 function countdown() {
 	stopwatch.innerHTML = (time===0) ? "Finished!" : time;
 	if (time===2 || time===1) {
